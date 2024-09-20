337cd62a8d712ebc4540d05879b803cffd535ff7
nyxiee10 <blessydel@gmail.com>
2024-09-21 00:47:15 +0800

    Add files via upload

---
+++ Identity_Class.cs
@@ -0,0 +1,5 @@
+﻿public class Identity
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
---
+++ User_Class.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+public class User
+{
+    private List<Identity> _users;
+
+    public void LoadUsersFromJson(string filePath)
+    {
+        // Read the JSON file and deserialize into a list of Identity objects
+        string jsonData = File.ReadAllText(filePath);
+        _users = JsonConvert.DeserializeObject<List<Identity>>(jsonData);
+    }
+
+    public bool IsValid(string username, string password)
+    {
+        // Check if any user matches the given username and password
+        foreach (var user in _users)
+        {
+            if (user.Username == username && user.Password == password)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
